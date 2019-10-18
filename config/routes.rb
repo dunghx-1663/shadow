6fc17e4326e7bf493c312dc86d8f558b30110aa9
haxdung <ha.xuan.dung@sun-asterisk.com>
2019-10-19 06:46:09 +0700

    add_gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :subjects
   resources :roles
   resources :admins
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  get    '/payment',  to: 'carts#payment'
+
+  get    'signup',  to: 'sessions#signup'
+  devise_for :users
 end
